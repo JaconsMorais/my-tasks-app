892b8fa766a5662fa181c4e2dcfb01cb8ea817a3
jaconsmorais <jaconssmorais@yahoo.com.br>
2022-09-24 14:51:43 -0400

    feat: base component for column using grid.

---
+++ src/components/base/BaseColumn.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-col v-bind="{ 'align-self': alignSelf, lg, md, sm, xl }">
+    <slot></slot>
+  </v-col>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "BaseColumn",
+  props: {
+    alignSelf: {
+      type: String,
+      default: "center",
+    },
+    lg: {
+      type: Number,
+      default: 12,
+    },
+    md: {
+      type: Number,
+      default: 12,
+    },
+    sm: {
+      type: Number,
+      default: 12,
+    },
+    xl: {
+      type: Number,
+      default: 12,
+    },
+  },
+});
+</script>
