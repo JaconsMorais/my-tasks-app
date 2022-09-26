3f8f62bdac8b8c5205ba62fc309776d9f43fc4fa
Jacons Morais <jaconssmorais@yahoo.com.br>
2022-09-25 20:10:47 -0400

    feat: BaseForm component to wrap all inputs in a form. The component has listeners for events like input and submit, allow user to submit the form on press enter. Validation in form should be implemented soon.

---
+++ src/components/base/BaseForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-form
+    v-bind="{ disabled, readonly }"
+    lazy-validation
+    @submit="onSubmit"
+    @input="onInput"
+  >
+    <v-col alignment="center" align-self="center">
+      <slot></slot>
+    </v-col>
+  </v-form>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "BaseForm",
+  props: {
+    disabled: Boolean,
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    onSubmit(v) {
+      this.$emit("onSubmit", v);
+    },
+    onInput(v) {
+      this.$emit("onInput", v);
+    },
+  },
+});
+</script>
