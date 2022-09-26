3ed127ecb29687bdce4c5c735ca0f63fcaabfc09
Jacons Morais <jaconssmorais@yahoo.com.br>
2022-09-25 20:08:04 -0400

    feat: BaseChip component to wrap v-chip

---
+++ src/components/base/BaseChip.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-chip v-bind="{label, color, textColor}">
+        <slot></slot>
+    </v-chip>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: "BaseChip",
+    props: {
+        label: Boolean,
+        color: {
+            type: String,
+            default: "primary"
+        },
+        textColor: {
+            type: String,
+            default: "black"
+        }
+    }
+})
+</script>
