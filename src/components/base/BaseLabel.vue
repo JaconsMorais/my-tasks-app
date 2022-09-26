5a6af820205707522eedcbf079dfa51a8e61cb62
Jacons Morais <jaconssmorais@yahoo.com.br>
2022-09-25 20:15:45 -0400

    feat: BaseLabel created to output text in HTML. Using this component the programmer can output whatever text and tag they want, the tag can be specified using as prop.

---
+++ src/components/base/BaseLabel.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container :tag="as" :class="{
+    [transform]: transform,
+    [decoration]: decoration,
+    [alignment]: alignment,
+    [fontWeight]: fontWeight,
+    [fontStyle]: fontStyle,
+  }" :style="{
+    color, 'font-size': fontSize, margin
+  }">
+    <slot></slot>
+  </v-container>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "BaseLabel",
+  props: {
+    transform: String,
+    decoration: String,
+    alignment: String,
+    fontWeight: String,
+    color: String,
+    fontStyle: String,
+    as: String,
+    fontSize: Number,
+    margin: String
+  },
+});
+</script>
