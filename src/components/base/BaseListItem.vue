194c6a89ca8a25941f889550a90e6f7dd759c5b8
Jacons Morais <jaconssmorais@yahoo.com.br>
2022-09-25 20:17:43 -0400

    feat: BaseListItem use a card to output an item in list, the item expect an title and subtitle prop, and has slot to add buttons for actions.

---
+++ src/components/base/BaseListItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card :color="color" :style="{ margin }">
+        <v-card-title> {{ title }} </v-card-title>
+
+        <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
+
+        <v-card-text>
+            <slot name="body"></slot>
+        </v-card-text>
+
+        <v-card-actions>
+            <slot name="footer"></slot>
+        </v-card-actions>
+    </v-card>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "BaseListItem",
+    props: {
+        color: {
+            type: String,
+            default: "white",
+        },
+        title: String,
+        subtitle: String,
+        margin: {
+            type: String,
+            default: "8px 0px",
+        },
+    },
+});
+</script>
