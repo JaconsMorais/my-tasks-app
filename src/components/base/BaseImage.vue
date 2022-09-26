070bc2076829114fc787fcf57850bfc51c7ce22d
Jacons Morais <jaconssmorais@yahoo.com.br>
2022-09-25 20:12:10 -0400

    feat: BaseImage component to add images in app, the component has src and srcset we can set url's to responsive images.

---
+++ src/components/base/BaseImage.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-img
+    v-bind="{
+      src,
+      srcset,
+      width,
+      alt,
+      'max-height': maxHeight,
+      'max-width': maxWidth,
+      'min-height': minHeight,
+      'min-width': minWidth,
+    }"
+    :style="{ margin }"
+    ><slot></slot
+  ></v-img>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "BaseImage",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    srcset: {
+      type: String,
+    },
+    width: {
+      type: String,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+    margin: String,
+    maxHeight: String,
+    maxWidth: String,
+    minHeight: String,
+    minWidth: String,
+  },
+});
+</script>
