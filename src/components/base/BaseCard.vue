ac5e87f9d41d8bdd8cc5aec936d4eba8fc5c47c1
Jacons Morais <jaconssmorais@yahoo.com.br>
2022-09-25 20:07:34 -0400

    feat: BaseCard component created to wrap v-card, the component has slots for title, body and footer content.

--- src/components/base/BaseCard.vue
+++ src/components/base/BaseCard.vue
@@ -8,9 +8,21 @@
       'min-height': minHeight,
       'min-width': minWidth,
     }"
+    :style="{
+      margin,
+      padding,
+    }"
   >
-    <slot></slot
-  ></v-card>
+    <v-card-title>
+      <slot name="header"></slot>
+    </v-card-title>
+    <v-divider v-if="$slots.header"></v-divider>
+    <v-card-text><slot></slot></v-card-text>
+    <v-divider v-if="$slots.footer"></v-divider>
+    <v-card-actions>
+      <slot name="footer"></slot>
+    </v-card-actions>
+  </v-card>
 </template>
 <script lang="ts">
 import Vue from "vue";
@@ -37,12 +49,14 @@ export default Vue.extend({
       type: String,
       default: "auto",
     },
+    margin: {
+      type: String,
+      default: "auto",
+    },
+    padding: {
+      type: String,
+      default: "8px",
+    },
   },
 });
 </script>
-<style scoped>
-.v-card {
-  margin: 16px;
-  padding: 8px;
-}
-</style>
